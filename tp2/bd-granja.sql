890f7982c6087605e964101dbb608eb9132ce4e3
nbmateus <mateusnicolasblas@gmail.com>
2018-10-31 17:13:04 -0300

    bd granja

---
+++ tp2/bd-granja.sql
@@ -0,0 +1,376 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bd-granja
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cabaña`
+--
+
+DROP TABLE IF EXISTS `cabaña`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cabaña` (
+  `cuit` varchar(45) NOT NULL,
+  `razonSocial` varchar(45) NOT NULL,
+  `idDomicilio` int(11) NOT NULL,
+  PRIMARY KEY (`cuit`),
+  KEY `fk_cabaña_Domicilio1_idx` (`idDomicilio`),
+  CONSTRAINT `fk_cabaña_Domicilio1` FOREIGN KEY (`idDomicilio`) REFERENCES `domicilio` (`idDomicilio`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cabaña`
+--
+
+LOCK TABLES `cabaña` WRITE;
+/*!40000 ALTER TABLE `cabaña` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cabaña` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+  `cuit` varchar(45) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `idDomicilio` int(11) NOT NULL,
+  PRIMARY KEY (`cuit`),
+  KEY `fk_cliente_Domicilio1_idx` (`idDomicilio`),
+  CONSTRAINT `fk_cliente_Domicilio1` FOREIGN KEY (`idDomicilio`) REFERENCES `domicilio` (`idDomicilio`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `domicilio`
+--
+
+DROP TABLE IF EXISTS `domicilio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domicilio` (
+  `idDomicilio` int(11) NOT NULL AUTO_INCREMENT,
+  `calle` varchar(45) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `localidad` varchar(45) NOT NULL,
+  `provincia` varchar(45) NOT NULL,
+  PRIMARY KEY (`idDomicilio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `domicilio`
+--
+
+LOCK TABLES `domicilio` WRITE;
+/*!40000 ALTER TABLE `domicilio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `domicilio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `factura`
+--
+
+DROP TABLE IF EXISTS `factura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `factura` (
+  `idFactura` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` int(11) NOT NULL,
+  `cantidad` varchar(45) DEFAULT NULL,
+  `cuitCliente` varchar(45) NOT NULL,
+  `codigoProducto` int(11) NOT NULL,
+  PRIMARY KEY (`idFactura`),
+  KEY `fk_Factura_Cliente1_idx` (`cuitCliente`),
+  KEY `fk_Factura_Producto1_idx` (`codigoProducto`),
+  CONSTRAINT `fk_Factura_Cliente1` FOREIGN KEY (`cuitCliente`) REFERENCES `cliente` (`cuit`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Factura_Producto1` FOREIGN KEY (`codigoProducto`) REFERENCES `producto` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `factura`
+--
+
+LOCK TABLES `factura` WRITE;
+/*!40000 ALTER TABLE `factura` DISABLE KEYS */;
+/*!40000 ALTER TABLE `factura` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `galpon`
+--
+
+DROP TABLE IF EXISTS `galpon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `galpon` (
+  `numero` int(11) NOT NULL,
+  `cantidadDeGallinas` int(11) DEFAULT NULL,
+  `codigoPlantel` int(11) DEFAULT NULL,
+  PRIMARY KEY (`numero`),
+  KEY `fk_Galpon_Plantel_idx` (`codigoPlantel`),
+  CONSTRAINT `fk_Galpon_Plantel` FOREIGN KEY (`codigoPlantel`) REFERENCES `plantel` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `galpon`
+--
+
+LOCK TABLES `galpon` WRITE;
+/*!40000 ALTER TABLE `galpon` DISABLE KEYS */;
+/*!40000 ALTER TABLE `galpon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `genetica`
+--
+
+DROP TABLE IF EXISTS `genetica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `genetica` (
+  `codigo` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `cuitCabaña` varchar(45) NOT NULL,
+  PRIMARY KEY (`codigo`,`cuitCabaña`),
+  KEY `fk_Genetica_Cabaña1_idx` (`cuitCabaña`),
+  CONSTRAINT `fk_Genetica_Cabaña1` FOREIGN KEY (`cuitCabaña`) REFERENCES `cabaña` (`cuit`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `genetica`
+--
+
+LOCK TABLES `genetica` WRITE;
+/*!40000 ALTER TABLE `genetica` DISABLE KEYS */;
+/*!40000 ALTER TABLE `genetica` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `planilla`
+--
+
+DROP TABLE IF EXISTS `planilla`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `planilla` (
+  `idPlanilla` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `cantidadHuevosObtenidos` int(11) DEFAULT NULL,
+  `cantidadGallinasMuertas` int(11) DEFAULT NULL,
+  `cantidadAlimento` float DEFAULT NULL,
+  `tipoAlimento` varchar(45) DEFAULT NULL,
+  `numeroGalpon` int(11) NOT NULL,
+  PRIMARY KEY (`idPlanilla`),
+  KEY `fk_Planilla_Galpon1_idx` (`numeroGalpon`),
+  CONSTRAINT `fk_Planilla_Galpon1` FOREIGN KEY (`numeroGalpon`) REFERENCES `galpon` (`numero`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `planilla`
+--
+
+LOCK TABLES `planilla` WRITE;
+/*!40000 ALTER TABLE `planilla` DISABLE KEYS */;
+/*!40000 ALTER TABLE `planilla` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `plantel`
+--
+
+DROP TABLE IF EXISTS `plantel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plantel` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `edadEntrada` int(11) DEFAULT NULL,
+  `codigoGenetica` int(11) NOT NULL,
+  `fechaEntrada` date DEFAULT NULL,
+  `precio` float DEFAULT NULL,
+  PRIMARY KEY (`codigo`,`codigoGenetica`),
+  KEY `fk_Plantel_Genetica1_idx` (`codigoGenetica`),
+  CONSTRAINT `fk_Plantel_Genetica1` FOREIGN KEY (`codigoGenetica`) REFERENCES `genetica` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `plantel`
+--
+
+LOCK TABLES `plantel` WRITE;
+/*!40000 ALTER TABLE `plantel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `plantel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `producto`
+--
+
+DROP TABLE IF EXISTS `producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `producto` (
+  `codigo` int(11) NOT NULL,
+  `precio` varchar(45) DEFAULT NULL,
+  `cantidad` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producto`
+--
+
+LOCK TABLES `producto` WRITE;
+/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'bd-granja'
+--
+
+--
+-- Dumping routines for database 'bd-granja'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `agregarCabaña` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ALLOW_INVALID_DATES,ERROR_FOR_DIVISION_BY_ZERO,TRADITIONAL,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `agregarCabaña`(cuit varchar(45), razonSocial varchar(45), calle varchar(45), numero int(11), localidad varchar(45), provincia varchar(45))
+BEGIN
+INSERT INTO `bd-granja`.`cabaña` (`cuit`, `razonSocial`, `calle`, `numero`, `localidad`, `provincia`) VALUES ( cuit, razonSocial, calle, numero, localidad, provincia);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `agregarGalpon` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ALLOW_INVALID_DATES,ERROR_FOR_DIVISION_BY_ZERO,TRADITIONAL,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `agregarGalpon`(num int, cantGallinas int, codPlantel int)
+BEGIN
+INSERT INTO `bd-granja`.galpon (numero, cantidadDeGallinas, Plantel_codigo) VALUES (num, cantidadDeGallinas, codPlantel);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `agregarGenetica` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ALLOW_INVALID_DATES,ERROR_FOR_DIVISION_BY_ZERO,TRADITIONAL,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `agregarGenetica`(codigo int(11), nombre varchar(45), cuitCabaña varchar(45))
+BEGIN
+INSERT INTO `bd-granja`.`genetica`(`codigo`, `nombre`, `Cabaña_cuit`)
+VALUES (codigo, nombre, cuitCabaña);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `agregarPlanilla` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `agregarPlanilla`(_fecha date, _cantHuevos int, _cantMuertas int, cantAlimento float, tipoAlimento varchar(45), _numGalpon int)
+BEGIN
+insert into `bd-granja`.planilla(`fecha`, `cantidadHuevosObtenidos`, `cantidadGallinasMuertas`, `cantidadAlimento`, `tipoAlimento`, `numeroGalpon`)
+values (_fecha, _cantHuevos, _cantMuertas, cantAlimento, tipoAlimento, _numGalpon);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `agregarPlantel` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ALLOW_INVALID_DATES,ERROR_FOR_DIVISION_BY_ZERO,TRADITIONAL,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `agregarPlantel`(codigo int(11), nombre varchar(45), edadEntrada int(11), genetica int(11), fechaEntrada date, precio float)
+BEGIN
+INSERT INTO `bd-granja`.`plantel`(`codigo`, `nombre`, `edadEntrada`, `Genetica_codigo`, `fechaEntrada`, `precio`)
+VALUES (codigo, nombre, edadEntrada, genetica, fechaEntrada, precio);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-31 17:10:18
