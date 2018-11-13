1a73ed157648117075c4302c92429d7d9cca3cb1
reypala <lucastisera@gmail.com>
2018-11-13 11:06:31 -0300

    Habemus TRIGGER

--- Scripts/triggers.sql
+++ Scripts/triggers.sql
@@ -1,18 +1,10 @@
 
+drop trigger tLogAltaPlanilla;
 delimiter $$
 
 create trigger tLogAltaPlanilla after update on planilla for each row
 begin
-	call agregarLog(fecha, 
-					cantidadHuevosObtenidos, 
-					cantidadGallinasMuertas,
-                    cantidadAlimento,
-                    tipoAlimento,
-                    galpon,
-                    novedades,
-                    idPlanilla,
-                    curdate(),
-                    1
-                    );
+	call agregarLog(OLD.fecha, old.cantidadHuevosObtenidos, OLD.cantidadGallinasMuertas, OLD.cantidadAlimento, OLD.tipoAlimento, OLD.galpon, OLD.novedades, OLD.idPlanilla, curdate(), 1);
 end$$
 delimiter ;
+
