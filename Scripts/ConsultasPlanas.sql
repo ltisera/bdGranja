06bd1af683b0e0cdd4494af98b1b1a14308b9fee
reypala <lucastisera@gmail.com>
2018-11-13 12:52:43 -0300

    TODO LO QUE TENGO HECHO

--- Scripts/ConsultasPlanas.sql
+++ Scripts/ConsultasPlanas.sql
@@ -5,4 +5,8 @@ begin
 	select plantel.idPlantel,galpon.idGalpon, sum(cantidadGallinasMuertas) as porPlantel from planilla inner join galpon on planilla.galpon = galpon.idGalpon inner join plantel on galpon.plantel = plantel.idPlantel group by galpon.idGalpon where planilla.fecha >= fechaA and planilla.fecha <= fechaB;
 
 end$$
-delimiter;
+delimiter ;
+
+
+
+call consultaGallinasMuertas("2018-12-1", "2018-12-30");
